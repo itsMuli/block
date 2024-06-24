ed97124149698e31f45849ef08f5d21c0272fc25
itsMuli <rosemwendwa163@gmail.com>
2024-06-24 12:08:28 +0300

    mongo uri extraction

--- contest/database.js
+++ contest/database.js
@@ -1,5 +1,6 @@
+const { config } = require('dotenv');
 const { MongoClient } = require('mongodb');
-const uri = 'mongodb+srv://rose:bobathebear@cluster0.6vksxc0.mongodb.net/'; // Connection URL
+const uri = process.env.MONGO_URI; // Connection URL
 const dbName = 'voting'; // Database Name
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
